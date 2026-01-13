a92fa30680a6a854275f1dfb4db1fc3052ece2d9
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-01-13 21:30:17 +0800

    design ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,30 @@
 import { useState } from "react";
 
 export default function Home() {
+  return <Grocery />;
+}
+
+const Grocery = () => {
+  return <GroceryList />;
+};
+
+const GroceryList = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className=" h-screen  bg-[#f9fafc]">
+      <div className="flex justify-center">
+        <div className="bg-[#ffffff] w-150 mt-50 flex flex-col justify-center items-center p-8 rounded-xl shadow-md">
+          <h1 className="text-3xl mb-4">Grocery Bud</h1>
+          <div className="px-4 py-2">
+            <input
+              className="border border-gray-300 w-[300px] h-[30px]"
+              type="text"
+            />
+            <button className="bg-[#07b6d5] text-[#d0f6fa] w-[100px] h-[30px] rounded-r-lg">
+              Add item
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-Home();
+};
